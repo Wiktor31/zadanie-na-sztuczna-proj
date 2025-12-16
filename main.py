ad87189bf1fa461da6950107d208ac182816fcb2
Wiktor Mazanka <wiktor.mazanka1234@interia.pl>
2025-12-16 23:29:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import tkinter as tk
 import clips
 from gui import asking_questions
 
-
+#setting up enviroments
 env=clips.Environment()
 env.load("Jobs_in_film_industry.clp")
 env.load("rules.clp")
-message = ""
 env.reset()
 env.run()
 root = tk.Tk()
 root.title("Simple Tkinter Example")
-root.geometry("600x200")  # Width x Height
+root.geometry("600x200")  # size of window
 
 asking_questions(env ,root)
 
 
 root.mainloop()
+
